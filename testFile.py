03780e2b7189fe580196d4838834a44e3d019e58
purnimabilla16 <billapurnima123@gmail.com>
2025-01-05 19:58:45 +0530

    modified a file

--- testFile.py
+++ testFile.py
@@ -6,24 +6,7 @@ driver = webdriver.Remote(
 command_executor='http://localhost:4444/wd/hub',
 options=options
 )
-driver.get("https://www.browserstack.com/")
-driver.find_element_by_link_text("Get started free").click()
-seleniumDockerTest.py
-from selenium import webdriver
-import time
-print("Test Execution Started")
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-ssl-errors=yes')
-options.add_argument('--ignore-certificate-errors')
-driver = webdriver.Remote(
-command_executor='http://localhost:4444/wd/hub',
-options=options
-)
-#maximize the window size
-driver.maximize_window()
-time.sleep(10)
-#navigate to browserstack.com
-driver.get("https://www.browserstack.com/")
+driver.gewww.browserstack.com/")
 time.sleep(10)
 #click on the Get started for free button
 
